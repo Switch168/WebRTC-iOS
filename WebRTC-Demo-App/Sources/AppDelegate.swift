efa0dd07950e3955c2e74c450d2f0fbd9ecc5cb4
Andrew Tan <andrewvmail@gmail.com>
2019-03-26 16:45:12 -0700

    chore(*): cordova runs on device

--- WebRTC-Demo-App/Sources/AppDelegate.swift
+++ WebRTC-Demo-App/Sources/AppDelegate.swift
@@ -11,29 +11,36 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    private var window: UIWindow?
+//    private var window: UIWindow?
+    
+    var window: UIWindow?
     private let config = Config.default
     
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = self.buildMainViewController()
-        window.makeKeyAndVisible()
-        self.window = window
+//    func application(_ application: UIApplication,
+//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        let window = UIWindow(frame: UIScreen.main.bounds)
+//        window.rootViewController = self.buildMainViewController()
+//        window.makeKeyAndVisible()
+//        self.window = window
+//        return true
+//    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
         return true
     }
     
-    private func buildMainViewController() -> UIViewController {
-        let signalClient = SignalingClient(serverUrl: self.config.signalingServerUrl)
-        let webRTCClient = WebRTCClient(iceServers: self.config.webRTCIceServers)
-        let mainViewController = MainViewController(signalClient: signalClient,
-                                                    webRTCClient: webRTCClient)
-        let navViewController = UINavigationController(rootViewController: mainViewController)
-        navViewController.navigationBar.isTranslucent = false
-        if #available(iOS 11.0, *) {
-            navViewController.navigationBar.prefersLargeTitles = true
-        }
-        return navViewController
-    }
+//    private func buildMainViewController() -> UIViewController {
+//        let signalClient = SignalingClient(serverUrl: self.config.signalingServerUrl)
+//        let webRTCClient = WebRTCClient(iceServers: self.config.webRTCIceServers)
+//        let mainViewController = MainViewController(signalClient: signalClient,
+//                                                    webRTCClient: webRTCClient)
+//        let navViewController = UINavigationController(rootViewController: mainViewController)
+//        navViewController.navigationBar.isTranslucent = false
+//        if #available(iOS 11.0, *) {
+//            navViewController.navigationBar.prefersLargeTitles = true
+//        }
+//        return navViewController
+//    }
 }
 

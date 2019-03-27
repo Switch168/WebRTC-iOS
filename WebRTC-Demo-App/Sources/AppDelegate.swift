96911bca68fa591cca1b573ede0e667c3fc75b03
Andrew Tan <andrewvmail@gmail.com>
2019-03-26 23:03:56 -0700

    WIP: can now stack subview ontop of cordova

--- WebRTC-Demo-App/Sources/AppDelegate.swift
+++ WebRTC-Demo-App/Sources/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //    private var window: UIWindow?
     
     var window: UIWindow?
-    var config = Config.default
+//    var config = Config.default
     
 //    func application(_ application: UIApplication,
 //                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- WebRTC-Demo-App/Sources/CordovaPlugin.swift
+++ WebRTC-Demo-App/Sources/CordovaPlugin.swift
@@ -5,30 +5,43 @@ import AVFoundation
 @objc(CordovaPlugin) // This class must be accesible from Objective-C.
 class CordovaPlugin : CDVPlugin {
 
-    var config = Config.default;
-    var window: UIWindow?;
-    let rootViewController = AppDelegate.shared.rootViewController
+//    var config = Config.default;
+//    var window: UIWindow?;
+//    let rootViewController = AppDelegate.shared.rootViewController
     
     @objc(hello:) func hello(_ command: CDVInvokedUrlCommand) {
         NSLog("CordovaPlugin#hello()");
 
+        dump(self);
         
-        let newViewController = self.buildMainViewController();
-        rootViewController.navigationController?.pushViewController(newViewController, animated: true)
+        let alertController = UIAlertController(title: "Hey AppCoda", message: "What do you want to do?", preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        // Where to get viewController?
+        
+        viewController.present(alertController, animated: true, completion: nil)
+        //rootViewController.navigationController?.pushViewController(newViewController, animated: true)
         
     }
     
     
-    private func buildMainViewController() -> UIViewController {
-        let signalClient = SignalingClient(serverUrl: self.config.signalingServerUrl)
-        let webRTCClient = WebRTCClient(iceServers: self.config.webRTCIceServers)
-        let mainViewController = MainViewController(signalClient: signalClient,
-                                                    webRTCClient: webRTCClient)
-        let navViewController = UINavigationController(rootViewController: mainViewController)
-        navViewController.navigationBar.isTranslucent = false
-        if #available(iOS 11.0, *) {
-            navViewController.navigationBar.prefersLargeTitles = true
-        }
-        return navViewController
-    }
+//    private func buildMainViewController() -> UIViewController {
+//        NSLog("CordovaPlugin#hello()");
+//        dump(self);
+//        dump(self.config);
+//
+//
+//        let signalClient = SignalingClient(serverUrl: self.config.signalingServerUrl)
+//        let webRTCClient = WebRTCClient(iceServers: self.config.webRTCIceServers)
+//        let mainViewController = MainViewController(signalClient: signalClient,
+//                                                    webRTCClient: webRTCClient)
+//        let navViewController = UINavigationController(rootViewController: mainViewController)
+//        navViewController.navigationBar.isTranslucent = false
+//        if #available(iOS 11.0, *) {
+//            navViewController.navigationBar.prefersLargeTitles = true
+//        }
+//        return navViewController
+//    }
 }
